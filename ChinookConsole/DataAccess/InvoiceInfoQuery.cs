96bbb8af875d29423dfa1f4b515434d78903cc18
Brooke Lewis <belv2c@gmail.com>
2018-02-26 19:48:53 -0600

    finished invoice total query

--- ChinookConsole/DataAccess/InvoiceInfoQuery.cs
+++ ChinookConsole/DataAccess/InvoiceInfoQuery.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 using System.Data.SqlClient;
 using System.Configuration;
 using ChinookConsole.DataAccess.Models;
@@ -40,6 +36,41 @@ namespace ChinookConsole.DataAccess
                     salesAgentEmployees.Add(employee);
                 }
                 return salesAgentEmployees;
+            }
+        }
+
+        public List<InvoiceInfo> GetInvoiceTotal()
+        {
+            using (var connection = new SqlConnection(_connectionString))
+            {
+                connection.Open();
+
+                var cmd = connection.CreateCommand();
+                cmd.CommandText = @"select i.Total, e.firstname + ' ' + e.lastname as [SalesAgent], i.BillingCountry, c.FirstName + ' ' + c.LastName as [CustomerName]
+                                    from Employee e
+                                    join customer c on c.SupportRepId = e.EmployeeId
+                                    join invoice i on i.CustomerId = c.CustomerId
+                                    where e.title = 'Sales Support Agent'";
+
+
+              
+                var reader = cmd.ExecuteReader();
+
+                var InvoiceTotal = new List<InvoiceInfo>();
+
+                while (reader.Read())
+                {
+                    var invoiceTotalInfo = new InvoiceInfo
+                    {
+                        CustomerName = reader["CustomerName"].ToString(),
+                        Total = double.Parse(reader["Total"].ToString()),
+                        SalesAgent = reader["SalesAgent"].ToString(),
+                        BillingCountry = reader["BillingCountry"].ToString()
+                    };
+
+                    InvoiceTotal.Add(invoiceTotalInfo);
+                }
+                return InvoiceTotal;
             }
         }
     }
--- ChinookConsole/Program.cs
+++ ChinookConsole/Program.cs
@@ -17,11 +17,11 @@ namespace ChinookConsole
                     case '0':
                         run = false;
                         break;
+
                     case '1':
                         Console.Clear();
                         var invoiceInfoQuery = new InvoiceInfoQuery();
                         var invoices = invoiceInfoQuery.GetAgent();
-                        //var invoiceDetails = invoiceInfoQuery.GetInvoiceInfo();
 
                         Console.WriteLine("Sales agents and their corresponding invoice id's:");
 
@@ -31,13 +31,29 @@ namespace ChinookConsole
                         }
                         Console.ReadLine();
                         break;
+
+                    case '2':
+                        Console.Clear();
+                        var invoiceInfoQuery2 = new InvoiceInfoQuery();
+                        var invoiceTotals = invoiceInfoQuery2.GetInvoiceTotal();
+
+                        Console.WriteLine("Invoice Total, customer name, country and sales agent names for all invoices:");
+
+                        foreach (var invoice in invoiceTotals)
+                        {
+                            Console.WriteLine($"Invoice Total: {invoice.Total}, Sales Agent Name: {invoice.SalesAgent}, Customer Info: {invoice.CustomerName}, {invoice.BillingCountry}");
+                        }
+                        Console.ReadLine();
+                        break;
                 }
             }
+
 
             ConsoleKeyInfo MainMenu()
             {
                 View mainMenu = new View()
                         .AddMenuOption("Show the invoices associated with each sales agent")
+                        .AddMenuOption("Show the invoice total, customer name, country and sales agent name for all invoices")
                         .AddMenuText("Press 0 to exit.");
 
                 Console.Write(mainMenu.GetFullMenu());
