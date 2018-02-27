2f318e67cc05911f4b57cf33a89ac2a2cda502b7
Brooke Lewis <belv2c@gmail.com>
2018-02-26 18:40:19 -0600

    added console menu and completed query 1

--- ChinookConsole/Program.cs
+++ ChinookConsole/Program.cs
@@ -12,7 +12,49 @@ namespace ChinookConsole
     {
         static void Main(string[] args)
         {
-           var invoiceInfoQuery = new InvoiceInfoQuery();
+            var run = true;
+            while (run)
+            {
+                ConsoleKeyInfo userInput = MainMenu();
+
+                switch (userInput.KeyChar)
+                {
+                    case '0':
+                        run = false;
+                        break;
+                    case '1':
+                        Console.Clear();
+                        var invoiceInfoQuery = new InvoiceInfoQuery();
+                        var invoices = invoiceInfoQuery.GetAgent();
+                        //var invoiceDetails = invoiceInfoQuery.GetInvoiceInfo();
+
+                        Console.WriteLine("");
+
+                        foreach (var invoice in invoices)
+                        {
+                            Console.WriteLine($"Sales Rep: {invoice.Name}, Invoice ID: {invoice.InvoiceId}");
+                        }
+
+                        Console.ReadLine();
+                        break;
+                }
+            }
+
+            ConsoleKeyInfo MainMenu()
+            {
+                View mainMenu = new View()
+                        .AddMenuOption("Show the invoices associated with each sales agent")
+                        .AddMenuText("Press 0 to exit.");
+
+                Console.Write(mainMenu.GetFullMenu());
+                ConsoleKeyInfo userOption = Console.ReadKey();
+                return userOption;
+            }
         }
     }
 }
+
+
+
+
+
